e4f2e5f05e9f61278541e536d6518d7bf14bfde2
Gunjan kumar <60321241+iamgunjan-ai@users.noreply.github.com>
2023-08-20 18:50:11 +0000

    variable

---
+++ 01_variable.js
@@ -0,0 +1,10 @@
+const acountid= 123454
+let acountemail="gunjankusha@gmail.com"
+var accountpassword ="11232"
+acountcity = "patna"
+acountemail="gunjankusha2@gmail.com"
+accountpassword ="11232232"
+acountcity = "gaya"
+
+console.log(acountid);
+console.table([acountemail,accountpassword,acountcity]);
