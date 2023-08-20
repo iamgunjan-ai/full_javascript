e72d8dc849ffd3daec9917160f6a59dd605c833a
Gunjan kumar <60321241+iamgunjan-ai@users.noreply.github.com>
2023-08-20 19:17:15 +0000

    datatypes

---
+++ 02_datatypes.js
@@ -0,0 +1,19 @@
+
+let name= "gunjan" //string data type
+
+let age = 18 //integer data types
+let isloggedid= false //boolean  true or false
+
+//Null => standalone value
+//undefine =>  let state not assin any value
+
+//symbol => unique
+
+// object 
+console.log(typeof(name));
+console.log(typeof(age));
+console.log(typeof (null));
+
+
+
+
