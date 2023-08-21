108b5727a141a3710e906b59384396255558891c
Gunjan kumar <60321241+iamgunjan-ai@users.noreply.github.com>
2023-08-21 16:31:40 +0000

    string

---
+++ string.js
@@ -0,0 +1,24 @@
+const name = "gunajn"
+const repcount = 10
+console.log(name + repcount +"value");
+
+const gamename=new String('gunjankumar')
+console.log(gamename);
+
+console.log(gamename[2]);
+console.log(gamename[1]);
+
+console.log(gamename.length);
+
+console.log(gamename.toUpperCase);
+
+console.log(gamename.toLowerCase);
+console.log(gamename.charAt(3));
+
+console.log(gamename.indexOf('k'));
+
+const newstring=gamename.substring(0.4);
+
+const anotherstaing=gamename.slice(-1);
+
+
