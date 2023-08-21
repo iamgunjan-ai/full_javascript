56120fac7a4d586f9cac7dccf1611a5f1feb3226
Gunjan kumar <60321241+iamgunjan-ai@users.noreply.github.com>
2023-08-21 16:52:07 +0000

    number_math

---
+++ number_math.js
@@ -0,0 +1,31 @@
+const score = 400
+console.log(score);
+
+const bale=new Number(100);
+console.log(bale);
+
+console.log(bale.toFixed(1));
+
+console.log(bale.toPrecision(1));
+
+const num = 100000
+console.log(num.toLocaleString('en-IN'));
+
+// math
+
+console.log("Math");
+
+console.log(Math.abs(-4));
+console.log(Math.round(4.6));
+
+console.log(Math.ceil(4.2));
+
+console.log(Math.floor(4.2));
+
+console.log(Math.min(4,5,6,1,2));
+
+console.log(Math.max(4,8.9,6));
+
+
+
+
