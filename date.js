e3e940f515586895dcf9f576425326b505cb976c
Gunjan kumar <60321241+iamgunjan-ai@users.noreply.github.com>
2023-08-21 17:19:04 +0000

    date

---
+++ date.js
@@ -0,0 +1,11 @@
+let mydate = new Date;
+console.log(mydate.toString());
+
+console.log(mydate.toLocaleString());
+
+console.log(typeof mydate);
+
+let myTimestamp = Date.now()
+console.log(myTimestamp);
+//getmonth
+//get day;
