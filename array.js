7efbd4471201a2b3a99e30e12478e137e99ff43b
Gunjan kumar <60321241+iamgunjan-ai@users.noreply.github.com>
2023-08-22 13:34:36 +0000

    array operation

---
+++ array.js
@@ -0,0 +1,17 @@
+const arr=[2,8,9,6,7,4]
+
+console.log(arr[1]);
+console.log(arr.reverse());
+console.log(arr.sort());
+// console.log(max.arr());
+// console.log(min.arr());
+
+arr.push(10)
+arr.push(11);
+console.log(arr);
+
+arr.pop()
+console.log(arr);
+
+
+
---
+++ array2.js
@@ -0,0 +1,8 @@
+const arr1= [5,9,8,3,6]
+
+const arr2 = [9,6,4,5]
+
+arr1.push(arr2)
+console.log(arr1);
+
+
