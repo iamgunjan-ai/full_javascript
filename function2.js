2aa2d1fd171e516ac4ba101d1b5dadcabe1634b1
Gunjan kumar <60321241+iamgunjan-ai@users.noreply.github.com>
2023-08-24 05:36:39 +0000

    function2

---
+++ function2.js
@@ -0,0 +1,16 @@
+function addcartprice(num1){
+    return num1
+}
+console.log(addcartprice(200,100,500));
+
+function addcartprice(...num1){
+    return num1
+}
+console.log(addcartprice(200,100,500));
+
+const user = {
+    username : "gunjan",
+    age:22
+}
+function handleuserobject(anyobject){
+}
