6307c0055fe7b0b322cc447e15b3550d63dc5d2b
Gunjan kumar <60321241+iamgunjan-ai@users.noreply.github.com>
2023-08-24 04:48:57 +0000

    object3

---
+++ objectApi.js
@@ -0,0 +1,16 @@
+const course={
+    coursename:" js in hindi",
+    prince:"999",
+    courseinstructor: "hitesh"
+}
+console.log(courseinstructor)=course
+//de structure
+const{courseinstructor: instructor}=course
+console.log(instructor);
+
+// Api
+// {
+//     name:"gunjan",
+//     coursename: "js",
+//     price:"free"
+// }
