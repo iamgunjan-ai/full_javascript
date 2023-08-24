6d9f63497e8c44c0619683bd5f4c4b46d5571d88
Gunjan kumar <60321241+iamgunjan-ai@users.noreply.github.com>
2023-08-24 06:26:57 +0000

    scope

---
+++ scope.js
@@ -0,0 +1,31 @@
+// let a =10
+// const b=20
+// var c=30 
+//scope {}
+// var c=300
+
+// if(true){
+//     let a =10
+//     const b=20
+//     var c=30  
+// }
+// // console.log(a);
+// // console.log(b)
+// console.log(c);
+
+function one(){
+    const username ="gunjan"
+
+    function two(){
+        const website="youtube"
+        console.log(username);
+    }
+   // console.log(website);
+    two();
+}
+one();
+
+// if(true){
+//     const username:"gunjankumar",
+    
+// }
